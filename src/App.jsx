ec9b05be67c03c2cd025007eb43b55e15aca9c61
Hugo Roosevelt <hugoroosevelt@icloud.com>
2026-03-27 14:01:57 -0600

    force map resize after load (fix hard refresh)

--- src/App.jsx
+++ src/App.jsx
@@ -139,7 +139,9 @@ useEffect(() => {
           "circle-opacity": 0.8,
         },
       });
-
+      setTimeout(() => {
+    map.resize();
+    }, 300);
       console.log("MAP FULLY STABLE ✅");
     });
   };
